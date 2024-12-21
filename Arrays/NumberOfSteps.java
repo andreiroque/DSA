c120a977fa739e28282bf348db373dd39281ce26
Andrei Roque <roqueandrei0316@gmail.com>
2024-12-21 21:47:26 +0800

    Add a boilerplate for Number of steps to reduce a number to zero

---
+++ Arrays/NumberOfSteps.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class NumberOfSteps {
+  
+  public static int numberOfStepsToReduceANumberToZero(int num){
+    int steps = 0;
+
+
+    return steps;
+  }
+  
+  public static void main(String[] args){
+    
+    
+
+  }
+
+}
