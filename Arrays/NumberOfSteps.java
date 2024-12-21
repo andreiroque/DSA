7f54b77e85c2ecd30a04a922b0f3e93b2c14f251
Andrei Roque <roqueandrei0316@gmail.com>
2024-12-21 21:51:24 +0800

    Add functionality t the number of steps to reduce a number to zero

--- Arrays/NumberOfSteps.java
+++ Arrays/NumberOfSteps.java
@@ -5,13 +5,25 @@ public class NumberOfSteps {
   public static int numberOfStepsToReduceANumberToZero(int num){
     int steps = 0;
 
+    while(num != 0){
+      if(num % 2 == 0){
+        num /= 2;
+      }else{
+        num--;
+      }
+      steps++;
+    }
 
     return steps;
   }
   
   public static void main(String[] args){
     
-    
+    int num = 14; // Case 1 - success
+    // int num = 8; // Case 2 - success
+    // int num = 123; // Case 3 - success
+
+    System.out.println("Number of Steps: " + numberOfStepsToReduceANumberToZero(num));
 
   }
 
