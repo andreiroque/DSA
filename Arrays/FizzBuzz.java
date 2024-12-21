574d41075b459c572904ce0fc04358901e99ed64
Andrei Roque <roqueandrei0316@gmail.com>
2024-12-21 20:59:53 +0800

    Add boilerplate for FizzBuzz

---
+++ Arrays/FizzBuzz.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+
+  public List<String> fizzBuzz(int n){
+    List<String> li = new ArrayList<>();
+
+    return li;
+  }
+
+  public static void main(String[] args){
+
+    
+
+  }
+
+}
