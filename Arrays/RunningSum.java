860228d802646c1c111bfe7032ffdd0d20a00678
Andrei Roque <roqueandrei0316@gmail.com>
2024-12-21 20:57:22 +0800

    Add runningSum functionality

--- Arrays/RunningSum.java
+++ Arrays/RunningSum.java
@@ -6,6 +6,12 @@ public class RunningSum {
   public static int[] runningSum(int[] nums){
     int[] newArr = new int[nums.length];
 
+    int prev = 0;
+
+    for(int i = 0; i < nums.length; i++){
+      prev += nums[i];
+      newArr[i] = prev;
+    }
   
     return newArr;
   }
@@ -13,7 +19,14 @@ public class RunningSum {
 
   public static void main(String[] args){
 
+    int[] arr = {1,2,3,4}; // Case 1 - success
+    // int[] arr = {1,1,1,1,1}; // Case 2 - success
+    // int[] arr = {3,1,2,10,1}; // Case 3 - success
 
+    int[] result = runningSum(arr);
+    for(int i = 0; i < result.length; i++){
+      System.out.print(result[i] + " ");
+    }
 
   }
 
