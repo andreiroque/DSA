b8f20ef5588b3e3f471050c57081ee030cbaa68f
Andrei Roque <roqueandrei0316@gmail.com>
2024-12-21 21:08:27 +0800

    Add boilerplate to Richest Customer Wealth

---
+++ Arrays/RichestCustomerWealth.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+
+public class RichestCustomerWealth {
+
+  public static int maximumWealth(int[][] accounts){
+    int max = 0;
+
+    return max;
+  }
+
+
+  public static void main(String[] args){
+
+
+
+  }
+
+}
