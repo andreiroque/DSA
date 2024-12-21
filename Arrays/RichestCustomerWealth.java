ad5175a854e201854f0367478c1e889d910885e0
Andrei Roque <roqueandrei0316@gmail.com>
2024-12-21 21:34:44 +0800

    Add an alternative code for getting the max in Richest Customer Wealth

--- Arrays/RichestCustomerWealth.java
+++ Arrays/RichestCustomerWealth.java
@@ -12,6 +12,7 @@ public class RichestCustomerWealth {
         if(total > max){
           max = total;
         }
+        //max = Math.max(total, max); // I think this one is better than adding an if statement.
       }
     }
 
@@ -23,7 +24,7 @@ public class RichestCustomerWealth {
 
     int[][] accounts = {{1,2,3}, {3,2,1}}; // Case 1 - success;
     // int[][] accounts = {{1,5}, {7,3}, {3,5}}; // Case 2 - success;
-    // int[][] accounts = {{2,8,7}, {7,1,3}, {1,9,5}}; // Case 3 - success;
+    // int[][] accounts = {{2,8,7}, {7,1,3}, {1,9,5}}; // Case 3 - success;;
     int result = maximumWealth(accounts);
 
     System.out.print("Maximum Wealth: " + result);
