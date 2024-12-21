9b86c70ecd7fa68e5aacd65b6252e0858453c56b
Andrei Roque <roqueandrei0316@gmail.com>
2024-12-21 20:52:51 +0800

    Add boilerplate for Running Sum of 1d Array

---
+++ Arrays/RunningSum.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class RunningSum {
+
+
+  public static int[] runningSum(int[] nums){
+    int[] newArr = new int[nums.length];
+
+  
+    return newArr;
+  }
+
+
+  public static void main(String[] args){
+
+
+
+  }
+
+}
