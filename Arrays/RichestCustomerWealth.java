47471500d34364f9679605eabedc52c1bef0f84e
Andrei Roque <roqueandrei0316@gmail.com>
2024-12-21 21:17:41 +0800

    Add functionality to Richest Customer Wealth

--- Arrays/RichestCustomerWealth.java
+++ Arrays/RichestCustomerWealth.java
@@ -5,6 +5,15 @@ public class RichestCustomerWealth {
 
   public static int maximumWealth(int[][] accounts){
     int max = 0;
+    for(int i = 0; i < accounts.length; i++){
+      int total = 0;
+      for(int j = 0; j < accounts[i].length; j++){
+         total += accounts[i][j];
+        if(total > max){
+          max = total;
+        }
+      }
+    }
 
     return max;
   }
@@ -12,7 +21,12 @@ public class RichestCustomerWealth {
 
   public static void main(String[] args){
 
+    int[][] accounts = {{1,2,3}, {3,2,1}}; // Case 1 - success;
+    // int[][] accounts = {{1,5}, {7,3}, {3,5}}; // Case 2 - success;
+    // int[][] accounts = {{2,8,7}, {7,1,3}, {1,9,5}}; // Case 3 - success;
+    int result = maximumWealth(accounts);
 
+    System.out.print("Maximum Wealth: " + result);
 
   }
 
